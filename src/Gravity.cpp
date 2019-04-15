b104032b38eb0b8ad309424306316e91afaa7334
Bobby <bobby@00384fe957d394corl-2.local>
2019-04-15 20:59:08 +0100

    Boo I can't compile

--- src/Gravity.cpp
+++ src/Gravity.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 #include "Gravity.hpp"
 #include "Particle.hpp"
-#include "GeneralSetting.hpp"
+//#include "GeneralSetting.hpp"
 
 using namespace std;
 
 void Gravity(Particle& part1, Particle& part2){
   double frame_gap = 0.1;
-  double Force = (6.674e-11*part1.mass*part2.mass2)/((part1.pos_x-part2.pos_x)*(part1.pos_x-part2.pos_x)+(part1.pos_y-part2.pos_y)*(part1.pos_y-part2.pos_y));
+  double Force = (6.674e-11*part1.mass*part2.mass)/((part1.pos_x-part2.pos_x)*(part1.pos_x-part2.pos_x)+(part1.pos_y-part2.pos_y)*(part1.pos_y-part2.pos_y));
   double Acce1 = (Force/part1.mass);
   double Acce2 = (Force/part2.mass);
   double velocity1_y = Acce1*sin(atan((part1.pos_y-part2.pos_y)/(part1.pos_x-part2.pos_x)))*frame_gap + part1.vel_y;
