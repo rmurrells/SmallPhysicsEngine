4eaa34103f395e87db88d504622665af8f4decf6
chester755@googlemail.com <chester755@googlemail.com>
2019-04-15 19:48:43 +0100

    Added Gravity Two Body Calculator Not compiled

---
+++ inc/Gravity.hpp
@@ -0,0 +1,7 @@
+#ifndef GRAVITY_HPP
+#define GRAVITY_HPP
+#include "Particle.hpp"
+
+void Gravity(Particle& part1, Particle& part2);
+
+#endif
---
+++ src/Gravity.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <math.h>
+#include <iostream>
+#include "Gravity.hpp"
+#include "Particle.hpp"
+#include "GeneralSetting.hpp"
+
+using namespace std;
+
+void Gravity(Particle& part1, Particle& part2){
+  double frame_gap = 0.1;
+  double Force = (6.674e-11*part1.mass*part2.mass2)/((part1.pos_x-part2.pos_x)*(part1.pos_x-part2.pos_x)+(part1.pos_y-part2.pos_y)*(part1.pos_y-part2.pos_y));
+  double Acce1 = (Force/part1.mass);
+  double Acce2 = (Force/part2.mass);
+  double velocity1_y = Acce1*sin(atan((part1.pos_y-part2.pos_y)/(part1.pos_x-part2.pos_x)))*frame_gap + part1.vel_y;
+  double velocity1_x = Acce1*cos(atan((part1.pos_y-part2.pos_y)/(part1.pos_x-part2.pos_x)))*frame_gap + part1.vel_x;
+  double velocity2_y = Acce2*sin(atan((part1.pos_y-part2.pos_y)/(part1.pos_x-part2.pos_x)))*frame_gap + part2.vel_y;
+  double velocity2_x = Acce2*cos(atan((part1.pos_y-part2.pos_y)/(part1.pos_x-part2.pos_x)))*frame_gap + part2.vel_x;  
+
+  part1.vel_x = velocity1_x;
+  part1.vel_y = velocity1_y;
+  part2.vel_x = velocity2_x;
+  part2.vel_y = velocity2_y;
+  
+}
